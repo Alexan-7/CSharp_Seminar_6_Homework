4c989a6809fc3a8cb289fd342de54fa07d14ff5a
Sasha <brdgn@bk.ru>
2022-09-22 14:47:06 +0300

    first commit

---
+++ hw_41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите любое количество целых чисел через пробел: ");
+string line = Console.ReadLine ()!;
+string[] parts = line.Split(' ');
+int[] numbers = new int[parts.Length];          // перевести массив строк в массив чисел по [кол-ву в строке]
+
+for (int i = 0; i < numbers.Length; i++)        // в новый массив положить элементы старого массива...
+    numbers[i] = int.Parse(parts[i]);
+
+int count = 0;
+int zero = 0;
+
+foreach (int value in numbers)
+{
+    if (value > 0)
+        count++;
+    else if (value == 0)
+        zero++;
+}
+
+Console.WriteLine($"Кол-во положительных элементов: {count}, кол-во нулей: {zero}, ");
+Console.WriteLine($"Кол-во отрицательных элементов: {numbers.Length - count - zero}");
---
+++ hw_43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Программа найдёт точку пересечения двух прямых, заданных их уравнениями");
+Console.Write("Введите k1: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double d = Convert.ToDouble(Console.ReadLine());
+
+CheckingLines(a, b, c, d);
+
+void CheckingLines(double a, double b, double c, double d)
+{
+    if (a * c == -1)
+        Console.WriteLine("Ваши прямые перпендикулярны");
+    else if (a == c && b == d)
+        Console.WriteLine("Прямые совпадают");
+    else if (a == c)
+        Console.WriteLine("Точек пересечения нет, прямые параллельны");
+    else
+    {
+        double x = (d - b) / (a - c);   // чем уже видимость переменной,...
+        double y = a * x + b;           // ...тем меньше ошибок с ней
+        Console.WriteLine($"Абсцисса точки пересечения прямых: {x}, её ордината: {y}");
+    }
+}
+
